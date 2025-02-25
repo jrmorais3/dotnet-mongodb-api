f9dfd46b92a10052d2ad4127bd77c418628c56c0
Junior Morais <jrmorais3@gmail.com>
2025-02-25 05:13:31 -0300

    feat: add collection class

---
+++ dotnet-mongodb-api/Data/Collections/Infectado.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace dotnet_mongodb_api.Data.Collections
+{
+    public class Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+    {
+        public DateTime DataNascimento { get; set; } = dataNascimento;
+        public string Sexo { get; set; } = sexo;
+        public double Latitude { get; set; } = latitude;
+        public double Longitude { get; set; } = longitude;
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; } = new GeoJson2DGeographicCoordinates(longitude, latitude);
+    }
+}
